36508b01cdd923dfa415dc739539a4e1dbd31688
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-08-06 17:20:00 -0300

    Update NegociacoesView.js

--- js/app/views/NegociacoesView.js
+++ js/app/views/NegociacoesView.js
@@ -3,31 +3,40 @@ class NegociacoesView {
   constructor(elemento) {
 
     this._elemento = elemento;
-}
+  }
 
-  _template() {
+  _template(model) {
 
     return `
     <table class="table table-hover table-bordered">
-        <thead>
-            <tr>
-                <th>DATA</th>
-                <th>QUANTIDADE</th>
-                <th>VALOR</th>
-                <th>VOLUME</th>
-            </tr>
-        </thead>
-        <tbody>
-        </tbody>
-        <tfoot>
-        </tfoot>
-    </table>
-    `;
-  }
-  update() {
+                <thead>
+                    <tr>
+                        <th>DATA</th>
+                        <th>QUANTIDADE</th>
+                        <th>VALOR</th>
+                        <th>VOLUME</th>
+                    </tr>
+                </thead>
+                <tbody>                
+                  ${model.negociacoes.map(n => {
 
-    this._elemento.innerHTML = this._template();
-}
-  
+                    return `
+                      <tr>
+                        <td>${DateHelper.dataParaTexto(n.data)}</td>
+                        <td>${n.quantidade}</td>
+                        <td>${n.valor}</td>
+                        <td>${n.volume}</td>
+                    </tr>
+                    `
+                  }).join('')}
+                </tbody>
+         </table>
+        `;
+  }
+  update(model) {
+
+    this._elemento.innerHTML = this._template(model);
+  }
+
 }
 
