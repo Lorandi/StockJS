f902328265504cfd726d4097444ec94c8457d591
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-08-11 10:24:46 -0300

    add comments

--- js/app/views/MensagemView.js
+++ js/app/views/MensagemView.js
@@ -3,6 +3,10 @@ class MensagemView extends View {
   constructor(elemento) {
       super(elemento);
   }
+  // é desnecessário fazer o constructor pois o JS automaticamente considera o 
+  // constructor da classe pai.
+  // É necessário fazer o constructor na classe filho quando tiver um parametro 
+  // exclusivo da classe filho, o que não é o caso desta.
 
   template(model) {
 
